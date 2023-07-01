ff78981d7322c97260b0df2f13f45ef5bf99f8de
GreatHub1204 <grahasmin@gmail.com>
2023-07-02 02:56:27 +0900

    socket serve

---
+++ server.php
@@ -0,0 +1,41 @@
+<?php
+use Ratchet\MessageComponentInterface;
+use Ratchet\ConnectionInterface;
+use Ratchet\Server\IoServer;
+use Ratchet\Http\HttpServer;
+use Ratchet\WebSocket\WsServer;
+require __DIR__ . '/vendor/autoload.php';
+class WebSocketsServer implements MessageComponentInterface {
+    protected $clients;
+    public function __construct() {
+        $this->clients = new \SplObjectStorage;
+    }
+    public function onOpen(ConnectionInterface $conn) {
+        $this->clients->attach($conn);
+        echo "New connection! ({$conn->resourceId})\n";
+    }
+    public function onMessage(ConnectionInterface $from, $msg) {
+        foreach ($this->clients as $client) {
+            if ($from !== $client) {
+                $client->send($msg);
+            }
+        }
+    }
+    public function onClose(ConnectionInterface $conn) {
+        $this->clients->detach($conn);
+        echo "Connection {$conn->resourceId} has disconnected\n";
+    }
+    public function onError(ConnectionInterface $conn, \Exception $e) {
+        echo "An error has occurred: {$e->getMessage()}\n";
+        $conn->close();
+    }
+}
+$server = IoServer::factory(
+    new HttpServer(
+        new WsServer(
+            new WebSocketsServer()
+        )
+    ),
+    8089
+);
+$server->run();
